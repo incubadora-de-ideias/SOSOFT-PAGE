f78a5c5a4685d054e9f117afe462737a6b086a8b
lildemaran-ui <lildemaran@gmail.com>
2025-11-05 09:46:10 +0100

    fix: remover o basename do HashRouter em main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {  HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter basename="/SOSOFT-PAGE">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
